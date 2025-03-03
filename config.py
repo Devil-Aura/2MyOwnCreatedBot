96d4622e36b83137bb5766e05a1d0d92dd81425c
Devil-Aura <imh203356@gmail.com>
2025-03-04 01:15:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@
 BOT_TOKEN = "YOUR_MAIN_BOT_TOKEN"
 
 # MongoDB Configuration
-MONGO_URI = "mongodb://localhost:27017/"  # Replace with your MongoDB URI
-DATABASE_NAME = "telegram_bot_db"  # Name of the database
+MONGO_URI = "mongodb+srv://bogiv45946:Zrg5kF2o3wVBwNuU@cluster0.wqq56.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
+DATABASE_NAME = "Cluster0"  # Name of the database
 
 # Telegram Log Channel
-LOG_CHANNEL_ID = -1001234567890  # Replace with your Telegram channel ID (must be a public or private channel)
+LOG_CHANNEL_ID = -1002428537093 # Replace with your Telegram channel ID (must be a public or private channel)
 
 # Owner User ID
-OWNER_ID = 123456789  # Replace with your Telegram user ID
+OWNER_ID = 6040503076  # Replace with your Telegram user ID
